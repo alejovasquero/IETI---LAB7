64a7813a06a2a1cd7dc9fa70221c92af41cf114d
alejovasquero <alejo_704@hotmail.com>
2021-03-13 22:54:02 -0500

    :sparkles: :rocket: Init and first function deploy

---
+++ add-task/index.js
@@ -0,0 +1,31 @@
+var id = 0;
+var tasks = [];
+module.exports = async function (context, req) {
+    context.log('Adding a task to the planner');
+
+    const task = req.body;
+    // Implement the response
+
+    task.id = ++id;
+    
+    
+
+    if (!(task.description && task.responsible && task.status && task.dueDate)) {
+        const responseMessage = { response: "Invalid task" };
+
+        context.res = {
+            status: 400,
+            body: responseMessage
+        };
+    } else {
+        tasks.push(task)
+        const responseMessage = { response: task };
+
+        context.res = {
+            status: 201,
+            body: responseMessage
+        };
+    }
+    console.log(tasks);
+
+}
